48ae090d3895d4c1fc4abc1e6913d28361cfe68e
Crynowecer0 <crynowecer11@gmail.com>
2023-05-22 19:43:50 -0400

    render pokemon cards, need to refactor prop passing with pokemon data

--- react-props-pokedex/pokedex/src/Pokedex.js
+++ react-props-pokedex/pokedex/src/Pokedex.js
@@ -6,3 +6,30 @@ and renders a sequence of Pokecard components.*/
 // pokedex >>>
 // pokestats.map((p => id: name: ect))
 // app builds the page
+import pokemons from "./pokemonStats.js";
+import Pokecard from "./Pokecard.js";
+
+/**renders Pokecard components, passing each component a single pokemon object from
+ * the pokemons array
+ *
+ * pokemon object : {id, name, image, type, base_experience}
+ */
+function Pokedex() {
+  const myStyles = {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+  };
+
+  return (
+    <div style={myStyles}>
+      {pokemons.map((pokemon) => (
+        //TODO: ASK WHAT is best practice when passing whole objects as props??
+        <Pokecard {...pokemon} />
+      ))}
+    </div>
+  );
+}
+
+export default Pokedex;
